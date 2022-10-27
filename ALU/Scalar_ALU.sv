64e1d3f8f3cd322cde0a043b89be15a3e5b2b757
Brian Mhatre <bmhatre@wisc.edu>
2022-10-27 16:47:38 -0500

    updated scalar alu code

--- ALU/Scalar_ALU.sv
+++ ALU/Scalar_ALU.sv
@@ -1,14 +1,15 @@
 
-module alu (A, B, op, out, zf, sf, of);
+module alu (A, B, op, out, zf, sf, of, clk, rst_n);
 
 input [35:0] A, B;
 
 input [3:0] op
 
-output [35:0] out;
+output logic [35:0] out;
 
-output zf, sf, of;
+output logic nz, ez, lz, gz, le, ge;
 
+logic nz_next, ez_next, lz_next, gz_next, le_next, ge_next;
 
 always@(op) begin
     case(op) begin
@@ -24,7 +25,37 @@ always@(op) begin
     end
 end
 
+assign nz_next = out != 36'b0;
 
+assign ez_next = ~nz_next;
+
+assign lz_next = out[35];
+
+assign gz_next = ~lz_next;
+
+assign le_next = lz_next | ez_next;
+
+assign ge_next = gz_next | ez_next;
+
+always_ff@(posedge clk, negedge !rst_n) begin
+    if(!rst_n) begin
+       nz <= 0;
+       ez <= 0;
+       lz <= 0;
+       gz <= 0;
+       le <= 0;
+       ge <= 0;
+    end
+    else if(op[3] == 1'b1) begin
+        nz <= nz_next;
+        ez <= ez_next;
+        lz <= lz_next;
+        gz <= gz_next;
+        le <= le_next;
+        ge <= ge_next;
+    end
+
+end
 
 
 endmodule
