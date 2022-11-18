8176d0db45a95581fb6e0a7d9033993788454734
FluffyAlligator <51432141+FluffyAlligator@users.noreply.github.com>
2022-11-17 18:02:36 -0600

    Update fetch.sv

--- STAGES/fetch.sv
+++ STAGES/fetch.sv
@@ -1,12 +1,15 @@
 module fetch
 (
-  input logic clk, rst, pc_control, stall, halt, unhalt;
-  input logic [35:0] pc_branch;
-  input logic [31:0] instr; 
-  output [35:0] pc_next;
-  output is_running;
-  output[4:0] vread1, vread2;
+  input logic clk, rst, pc_control, stall, halt, unhalt, cache_stall,
+  input logic [35:0] pc_branch,
+  input logic [31:0] instr,
+  output [35:0] pc_next,
+  output is_running,
+  output[4:0] vread1, vread2
 );
+
+// intermediate value to calculate vread1
+logic vread1_inter;
 
 // pc_next_mux is the output of the mux determining pc_next
 logic [35:0] pc_next_mux, pc_plus_4, pc;
@@ -18,7 +21,7 @@ logic halt_reg, next_halt_reg;
 assign pc_next_mux = (pc_control) ? pc_branch : pc_plus_4;
 
 // causes pc to retain it's value if stall is asserted
-assign pc_next = (stall | caches_stall) ? pc : pc_next_mux;
+assign pc_next = (stall | cache_stall) ? pc : pc_next_mux;
 
 // is_running is high when the core is not halted
 assign is_running = ~halt_reg;
@@ -30,7 +33,7 @@ assign pc_plus_4 = pc + 4;
 assign next_halt_reg = (halt_reg & ~unhalt) | (~halt_reg & halt);
 
 // pc register
-always_ff @(posedge clk, posedge rst)
+always_ff @(posedge clk, posedge rst) begin
   if(rst)
     pc <= 0;				
   else
@@ -38,19 +41,22 @@ always_ff @(posedge clk, posedge rst)
 end
 
 // stores if core is halted or not
-always_ff @(posedge clk, posedge rst)
+always_ff @(posedge clk, posedge rst) begin
   if(rst)
     halt_reg <= 1'b0;			
   else
     halt_reg <= next_halt_reg;
 end
 
+// intermediate signal to decode vread1
+assign vread1_inter = (instr[31:25]==7'b0100100) |  // vdota
+		     (instr[31:25]==7'b0101101) |  // vsma
+		     (instr[31:25]==7'b0110110) |  // vcomp
+		     (instr[31:25]==7'b0010010);   // vstr
+
 // decodes the first read vector
-assign vread1 = (instr[31:25]==7'b0100100 |  // vdota
-		 instr[31:25]==7'b0101101 |  // vsma
-		 instr[31:25]==7'b0110110 |  // vcomp
-		 instr[31:25]==7'b0010010 |) // vstr
-		 ? insr[9:5] : instr[19:15];
+assign vread1 = (vread1_inter) ? instr[9:5] : instr[19:15];
+
 // "decodes" the second read register
 assign vread2 = instr[14:10];
 
