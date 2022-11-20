c66aa60e3f4c50ddcb8892354e3bbe102c6b3c48
FluffyAlligator <51432141+FluffyAlligator@users.noreply.github.com>
2022-11-20 16:24:03 -0600

    Update mem.sv

--- STAGES/mem.sv
+++ STAGES/mem.sv
@@ -8,9 +8,13 @@ input [127:0] cache_data;
 // w_type 00 means not a write, 01 means 32 bit write, 10 means 36 bit write, 11 means 128 bit write
 input [1:0] line, w_type;
 
-logic [63:0] requested_line_64
+output [127:0] vector_wb;
+output [35:0] register_wb;
+
+logic [63:0] requested_line_64;
 logic [31:0] requested_line;
 logic [35:0] out_32, out_36, mem_wb;
+
 
 // the following is a set of muxes that select the proper output datas to write back to the scalar and vector 
 // register files
@@ -20,7 +24,7 @@ assign out_32 = {4'h0, requested_line};
 assign out_36 = requested_line_64[35:0];
 assign vector_wb = cache_data;
 assign mem_wb = (w_type[1]) ? out_36 : out_32;
-assign register_wb = (mem_operation) ? mem_wb : alu_out;
+assign register_wb = (mem_operation) ? mem_wb : alu_data;
 
 
 endmodule
