7b7fc6a52067f91d3122c8c970681bbdde9fac0b
bmha2001 <bmhatre@wisc.edu>
2022-11-21 12:46:42 -0600

    update

--- Decode Modules/control_unit.sv
+++ Decode Modules/control_unit.sv
@@ -22,7 +22,8 @@ always_comb begin
     control.vector_read_register2 = inst[19:15];
     control.scalar_read_register1 = inst[19:15];
     control.scalar_read_register2 = inst[14:10];
-    control.write_register = inst[24:20];
+    control.vector_write_register = inst[24:20];
+    control.scalar_write_register = inst[24:20];
     control.vector_alu_op = '0;
     control.op_type = '0;
     control.w_type = '0;
@@ -38,6 +39,7 @@ always_comb begin
     control.branch_register = 0;
     control.store_pc = 0;
     control.alu_operands = 0;
+    control.imm_type = '0;
     case (op_code)
         halt: begin
             halt = 1;
@@ -50,32 +52,38 @@ always_comb begin
         jal: begin
             control.branch_jump = 2'b01;
             control.register_wr_en = 1;
-            control.write_register = 32'hFFFF;
+            control.scalar_write_register = 32'hFFFF;
             store_pc = 1;
         end
         jmpr: begin
             control.branch_jump = 2'b01;
             control.branch_register = 1;
+            control.imm_type = 4'b0001;
         end
         jalr: begin
             control.branch_jump = 2'b01;
             control.branch_register = 1;
             control.register_wr_en = 1;
-            control.write_register = 32'hFFFF;
-            store_pc = 1;
+            control.scalar_write_register = 32'hFFFF;
+            control.store_pc = 1;
+            control.imm_type = 4'b0001;
         end
         bi: begin
             control.branch_jump = 2'b10;
+            control.imm_type = 4'b0010;
         end
         br: begin
             control.branch_jump = 2'b10;
             control.branch_register = 1;
+            control.imm_type = 4'b0011;
         end
         lih: begin
             control.register_wr_en = 1;
+            control.imm_type = 4'b0100;
         end
         lil: begin
             control.register_wr_en = 1;
+            control.imm_type = 4'b0100;
         end
         ld32: begin
             control.register_wr_en = 1;
@@ -83,6 +91,7 @@ always_comb begin
             control.r_type = 2'b01;
             control.scalar_alu_op = 4'b0000;
             control.alu_operands = 1;
+            control.imm_type = 4'b0011;
         end
         ld36: begin
             control.register_wr_en = 1;
@@ -90,51 +99,117 @@ always_comb begin
             control.r_type = 2'b10;
             control.scalar_alu_op = 4'b0000;
             control.alu_operands = 1;
+            control.imm_type = 4'b0011;
         end
         st32: begin
             control.mem_write = 1;
             control.w_type = 2'b01;
             control.scalar_alu_op = 4'b0000;
             control.alu_operands = 1;
+            control.imm_type = 4'b0101;
         end
         st36: begin
             control.mem_write = 1;
             control.w_type = 2'b10;
             control.scalar_alu_op = 4'b0000;
             control.alu_operands = 1;
+            control.imm_type = 4'b0101;
         end
         vldi: begin
             control.mem_read = 1;
+            control.vector_wr_en = 1;
+            control.register_wr_en = 1;
             control.r_type = 2'b11;
-            control.
+            control.scalar_alu_op = 4'b0000;
+            control.alu_operands = 1;
+            control.imm_type = 4'b0110;
+            control.scalar_write_register = inst[19:15];
         end
         vsti: begin
+            control.mem_write = 1;
+            control.vector_wr_en = 1;
+            control.register_wr_en = 1;
+            control.w_type = 2'b11;
+            control.scalar_alu_op = 4'b0000;
+            control.alu_operands = 1;
+            control.imm_type = 4'b0110;
+            control.scalar_write_register = inst[19:15];
         end
         vldr: begin
+            control.mem_read = 1;
+            control.vector_wr_en = 1;
+            control.register_wr_en = 1;
+            control.r_type = 2'b11;
+            control.scalar_alu_op = 4'b0000;
+            control.alu_operands = 0;
+            control.scalar_write_register = inst[19:15];
         end
         vstr: begin
+            control.mem_write = 1;
+            control.vector_wr_en = 1;
+            control.register_wr_en = 1;
+            control.w_type = 2'b11;
+            control.scalar_alu_op = 4'b0000;
+            control.alu_operands = 0;
+            control.scalar_write_register = inst[19:15];
         end
         Addi: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 1;
+            control.scalar_alu_op = 4'b0000;
+            control.imm_type = 4'b0110;
         end
         Subi: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 1;
+            control.scalar_alu_op = 4'b0001;
+            control.imm_type = 4'b0110;
         end
         Andi: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 1;
+            control.scalar_alu_op = 4'b0010;
+            control.imm_type = 4'b0110;
         end
         Ori: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 1;
+            control.scalar_alu_op = 4'b0011;
+            control.imm_type = 4'b0110;
         end
         xori: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 1;
+            control.scalar_alu_op = 4'b0100;
+            control.imm_type = 4'b0110;
         end
         shli: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 1;
+            control.scalar_alu_op = 4'b0101;
+            control.imm_type = 4'b0110;
         end
         shri: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 1;
+            control.scalar_alu_op = 4'b0111;
+            control.imm_type = 4'b0110;
         end
         cmpi: begin
+            control.alu_operands = 1;
+            contol.scalar_alu_op = 4'b1000;
+            control.imm_type = 4'b0111;
         end
         alu: begin
+            control.register_wr_en = 1;
+            control.alu_operands = 0;
+            control.scalar_alu_op = inst[3:0];
         end
         Not: begin
+            control.register_wr_en = 1;
         end
         Falu: begin
+            
         end
         cmp: begin
         end
