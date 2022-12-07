9582e50f3950f1935336a411b2a7880c7ac88699
RedDot <osuji@wisc.edu>
2022-12-07 08:59:29 -0600

    gotten from main

--- memory/data_cache.sv
+++ memory/data_cache.sv
@@ -1,17 +1,20 @@
 module data_cache(w_index, r_index, w_tag, r_tag, w_line, r_line, w_data, flushtype, w_way,
-                  w_tagcheck, rst, clk, w, r, tag_out, data_out, way, hit, dirty);
+                  w_tagcheck, rst, clk, w, r, tag_out, data_out, way, hit, dirty, no_tagcheck_way, no_tagcheck_read);
 input logic [7:0] w_index, r_index;	// indexes for write and read operation
 input logic [17:0] w_tag, r_tag;	// the write tag and the tag to be compared for read
 input logic [5:0] w_line, r_line;	// desired line for write and read, this module only uses the first two bits of these
 input logic [127:0] w_data;		// data to be written in write operation
 input logic [1:0] flushtype, w_way;	// type of flush 11 = flushclean 10 = flushdirty 01 = flushline 00 = noflush
 					// w_way is the way to write
+input logic [1:0] no_tagcheck_way;
 input logic w_tagcheck, rst, clk, w, r; // w_tagcheck is a signal that signifies to the cache that this is a read being done for purposes of a tagcheck
+input logic no_tagcheck_read;
 output logic [17:0] tag_out;            // this is important because it means next cycle there will be a write to this address, since metadata is read
 output logic [127:0] data_out;          // once per cycle and needs to be used by the read that should be happenning simultaneously metadata needs to be updated
 output logic [1:0] way;                 // on the cycle that w_tagcheck is asserted
 output logic hit, dirty;
-
+output logic [3:0] dirty_array;
+	
 logic r_reg, fwd_metadata, fwd_data, fwd_metadata_reg, fwd_data_reg, fwd_tag, fwd_tag_reg;
 logic [9:0] rd_addr;
 logic [7:0] r_index_reg;
@@ -24,7 +27,7 @@ logic [71:0] block_tag_out, tag_fwded;
 logic [17:0] tag_reg, wr_tag;
 logic [17:0] tag_data [3:0];
 logic [127:0] data [3:0];
-logic [3:0] valid_array, dirty_array, fwd_way_reg_onehot;
+logic [3:0] valid_array, fwd_way_reg_onehot;
 logic [2:0] plru;
 logic [3:0] match;
 logic [1:0] victimway, fwd_way, fwd_way_reg;
@@ -33,7 +36,7 @@ data_blockram data_blockram(.clk(clk), .clk2(clk), .rd_addr(rd_addr), .wr_data(w
 	.wr_index(wr_index), .wr_en(w), .data_out(block_data_out));
 
 metadata_registers metadata(.clk(clk), .rst(rst), .rd_addr(r_index),.wr_data(metadata_in), 
-	.wr_index(r_index_reg), .wr_en(r_reg), .data_out(metadata_out));
+			    .wr_index(r_index_reg), .wr_en(r_reg & ~no_tagcheck_read), .data_out(metadata_out));
 
 tag_blockram tags(.tag_out(block_tag_out), .r_index(r_index), .w_index(w_index), .tag_in(w_tag), 
                  .wr_en(w), .clk1(clk), .clk2(clk));
@@ -57,7 +60,7 @@ next_metadata_comb next_metadata(.way(way), .plru(plru), .valid_array(valid_arra
                    ~fwd_way_reg[1] & fwd_way_reg[0], ~|fwd_way_reg};
 
   assign hit = |match;                                                            // hit is 1 if a tag  match is found
-  assign way = (hit) ? {(match[3] | match[2]), (match[1] | match[3])} : victimway;  // way, if a hit is the way of the hit on miss the way of victim
+  assign way = (no_tagcheck_read) ? no_tagcheck_way : (hit) ? {(match[3] | match[2]), (match[1] | match[3])} : victimway;  // way, if a hit is the way of the hit on miss the way of victim
   assign plru = metadata_fwded[2:0];
   assign dirty_array = metadata_fwded[6:3];                                       // array of dirty values for 4 ways of cache
   assign valid_array = metadata_fwded[10:7];                                      // array of valid values for 4 ways of cache
@@ -71,7 +74,7 @@ next_metadata_comb next_metadata(.way(way), .plru(plru), .valid_array(valid_arra
 	         (way[0]) ? tag_data[1] : tag_data[0];
 
   assign victimway[1] = (~plru[2] & (valid_array[1] & valid_array[0])) | ~(valid_array[3] & valid_array[2]);                            // caclulates way of victim
-  assign victimway[0] = (((~plru[2]) ? ~plru[1] : ~plru[0]) & (valid_array[2] & valid_array[0])) | ~(valid_array[3] & valid_array[1]);
+  assign victimway[0] = (((~plru[2]) ? ~plru[1] : ~plru[0]) & (valid_array[2] & valid_array[0])) | (~(valid_array[3] & valid_array[1]) & ~(valid_array[3] & ~valid_array[2]));
 
   assign fwd_metadata = (r_index==r_index_reg);                                 // finds when metadata needs to be forwarded
   assign metadata_fwded = (fwd_metadata_reg) ? metadata_reg : metadata_out;     // fwd logic for metadata
