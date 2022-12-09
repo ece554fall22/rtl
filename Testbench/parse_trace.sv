cc719af935425ef055f3cef2d08d413103b446b9
garciacalder <garciacalder@ad.engr.wisc.edu>
2022-12-09 12:32:17 -0600

    created a few more lines

--- Testbench/parse_trace.sv
+++ Testbench/parse_trace.sv
@@ -6,12 +6,24 @@ initial begin
     // String variable to hold the line of the file
     string line;
 
-    bit [10:0] current_PC;
+    // first line
+    bit [35:0] PC;
     bit [31:0] inst;
-    bit [4:0] curr_reg;
-    bit [35:0] curr_reg_value;
-    int imm;
-    bit [35:0] wb_reg_value; 
+
+    // second line
+    bit [4:0] rD;
+    bit [35:0] rD_value;
+    bit [24:0] imm;
+    bit [4:0] rA;
+    bit [35:0] rA_value;
+    bit [4:0] rB;
+    bit [35:0] rB_value;
+
+    // third line
+    bit [4:0] wb_reg;
+    bit [35:0] wb_reg_value;
+
+
 
     // Open the tracer file in the current folder with "read" permission
     // fd = 0 if file doesn't exist
@@ -26,47 +38,56 @@ initial begin
         $fgets(line, fd);
 
         // first line
-        if ($sscanf(line, "*** %x: %x ***", current_PC, inst) == 2) {
-            // TODO: do something
-        }
+        if (line.substr(0,2) == "***") begin
+
+            $sscanf(line, "*** %x: %x ***", PC, inst);
+        end
 
 
         // second line
-        else if(line.substr(0,5) == "inputs") {
-            // TODO: do something
-        }
+        else if(line.substr(0,5) == "inputs") begin
+            // rD with imm
+            if ($sscanf("inputs: rD=r%x=%x imm=%x", rD, rD_value, imm) == 3) begin
+            end
+
+            // rD with rA and rB
+            else if ($sscanf("inputs: rD=r%x=%x rA=r%x=%x rB=r%x=%x", rD, rD_value, rA, rA_value, rB, rB_value) == 6) begin
+            end
+
+            // TODO: vector inputs
+        end
         
 
         // third line
-        else if(line.substr(0,15) == "scalar_writeback") {
-            // TODO: do something
-        }
+        else if(line.substr(0,15) == "scalar_writeback") begin
+            $sscanf("scalar_writeback: r%x=%x", wb_reg, wb_reg_value);
+        end
 
-        else if(line.substr(0,15) == "vector_writeback") {
-            // TODO: do something
-        }
+        else if(line.substr(0,15) == "vector_writeback") begin
+            $sscanf("vector_writeback: v%x = %x", wb_reg, wb_reg_value);
+        end
 
-        else if(line.substr(0,10) == "scalar_load") {
+        else if(line.substr(0,10) == "scalar_load") begin
             // TODO: do something
-        }
+        end
 
-        else if(line.substr(0,11) == "scalar_store") {
+        else if(line.substr(0,11) == "scalar_store") begin
             // TODO: do something
-        }
+        end
 
-        else if(line.substr(0,10) == "vector_load") {
+        else if(line.substr(0,10) == "vector_load") begin
             // TODO: do something
-        }
+        end
 
-        else if(line.substr(0,11) == "vector_store") {
+        else if(line.substr(0,11) == "vector_store") begin
             // TODO: do something
-        }
+        end
 
 
         // fourth line
-        else if(line.substr(0,2) == "asm") {
+        else if(line.substr(0,2) == "asm") begin
             // TODO: do something
-        }
+        end
 
         // debugging
         $display("%x", inst);
