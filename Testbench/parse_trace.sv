80286ca898f6e72068d38f3b55225171a97d436c
Leo Garcia Calderon <garciacalder@wisc.edu>
2022-12-09 16:11:32 -0600

    updating parse trace and adding new trace file

--- Testbench/parse_trace.sv
+++ Testbench/parse_trace.sv
@@ -11,8 +11,10 @@ initial begin
     bit [31:0] inst;
 
     // second line
+    integer rD_int;
     bit [4:0] rD;
     bit [35:0] rD_value;
+    integer imm_int;
     bit [24:0] imm;
     bit [4:0] rA;
     bit [35:0] rA_value;
@@ -27,14 +29,15 @@ initial begin
 
     // Open the tracer file in the current folder with "read" permission
     // fd = 0 if file doesn't exist
-    fd = $fopen ("I:\\ece554\\rtl\\Testbench\\randscalar0.trace", "r");
+    fd = $fopen ("I:\\ece554\\rtl\\Testbench\\scalar_pipeline_test.trace", "r");
     if (fd) $display("Trace file opened successfully : %0d", fd);
     else begin
         $display("File not opened successfully : %0d", fd);
         $fclose(fd);
         $stop;
     end
-    while (!$feof(fd)) begin
+    // while (!$feof(fd)) begin
+    for(int i = 0; i < 4; i++) begin
         $fgets(line, fd);
 
         // first line
@@ -45,9 +48,9 @@ initial begin
 
 
         // second line
-        else if(line.substr(0,5) == "inputs") begin
+        else if(line.substr(0,9) == "    inputs") begin
             // rD with imm
-            if ($sscanf("inputs: rD=r%x=%x imm=%x", rD, rD_value, imm) == 3) begin
+            if ($sscanf(line, "    inputs: rD=r%x=%x imm=%d", rD, rD_value, imm) == 3) begin
             end
 
             // rD with rA and rB
@@ -59,12 +62,12 @@ initial begin
         
 
         // third line
-        else if(line.substr(0,15) == "scalar_writeback") begin
-            $sscanf("scalar_writeback: r%x=%x", wb_reg, wb_reg_value);
+        else if(line.substr(0,19) == "    scalar_writeback") begin
+            $sscanf(line, "    scalar_writeback: rD=r%x=%x", wb_reg, wb_reg_value);
         end
 
-        else if(line.substr(0,15) == "vector_writeback") begin
-            $sscanf("vector_writeback: v%x = %x", wb_reg, wb_reg_value);
+        else if(line.substr(0,19) == "    vector_writeback") begin
+            $sscanf(line, "    vector_writeback: v%x = %x", wb_reg, wb_reg_value);
         end
 
         else if(line.substr(0,10) == "scalar_load") begin
@@ -90,7 +93,6 @@ initial begin
         end
 
         // debugging
-        $display("%x", inst);
         // $display("Line: %s", line);
     end
     
