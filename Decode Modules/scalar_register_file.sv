2f7e2389db5db3e9abf55d16a80de227a42dca4c
Brian Mhatre <bmhatre@wisc.edu>
2022-12-10 15:00:06 -0600

    bug fixes

--- Decode Modules/scalar_register_file.sv
+++ Decode Modules/scalar_register_file.sv
@@ -3,9 +3,9 @@ module register_file(read1, read2, write_addr, write_data, we, data1, data2, clk
 input [35:0] write_data;
 input [4:0] read1, read2, write_addr;
 input clk, rst_n, we;
-output logic data1, data2;
+output logic [35:0] data1, data2;
 
-logic reg_data1, reg_data2;
+logic [35:0] reg_data1, reg_data2;
 
 ram_single r1 (.q(write_data), .a_in(read1), .a_out(write_addr), .d(reg_data1), .we(we), .clk(clk));
 ram_single r2 (.q(write_data), .a_in(read1), .a_out(write_addr), .d(reg_data2), .we(we), .clk(clk));
