a1b973eb12c37ec50841015ebcf8d866e989c41d
bmha2001 <bmhatre@wisc.edu>
2022-12-10 18:59:19 -0600

    core working

--- Decode Modules/scalar_register_file.sv
+++ Decode Modules/scalar_register_file.sv
@@ -7,8 +7,8 @@ output logic [35:0] data1, data2;
 
 logic [35:0] reg_data1, reg_data2;
 
-ram_single r1 (.q(write_data), .a_in(read1), .a_out(write_addr), .d(reg_data1), .we(we), .clk(clk));
-ram_single r2 (.q(write_data), .a_in(read1), .a_out(write_addr), .d(reg_data2), .we(we), .clk(clk));
+ram_single r1 (.q(reg_data1), .a_in(write_addr), .a_out(read1), .d(write_data), .we(we), .clk1(clk), .clk2(~clk));
+ram_single r2 (.q(reg_data2), .a_in(write_addr), .a_out(read2), .d(write_data), .we(we), .clk1(clk), .clk2(~clk));
 
 assign data1 = (we & (read1 == write_addr)) ? write_data : reg_data1;
 assign data2 = (we & (read2 == write_addr)) ? write_data : reg_data2;
